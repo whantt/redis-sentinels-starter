d7d6be610d466fab5e7a085cad2651875e317a62
liuhaoyang <liuhaoyang@terminus.io>
2018-07-17 00:53:19 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # redis-sentinels-starter
 Redis哨兵集群快速启动。
+
+```
+sh redis-sentinels-starter.sh
+```
+
+启动 
+- 1个`master`节点，监听`6379`端口
+- 2个`slave`节点，监听`6380`,`6381`端口
+- 3个`sentinel`节点，监听`10859`,`10861`,`10862`端口
+- requirepass `123456`
---
+++ redis-sentinels-starter.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+ROOT_DIR=`pwd`
+TMP_DIR=`echo ${ROOT_DIR}/tmp`
+BIN_DIR=`echo ${ROOT_DIR}/bin`
+rm -rf ${TMP_DIR}
+mkdir -p ${TMP_DIR}
+cd ${TMP_DIR}
+wget http://download.redis.io/releases/redis-4.0.10.tar.gz
+tar -xzvf redis-4.0.10.tar.gz
+cd redis-4.0.10
+make
+mkdir -p ${BIN_DIR}
+mv -f src/redis-benchmark ${BIN_DIR}/redis-benchmark
+mv -f src/redis-cli ${BIN_DIR}/redis-cli
+mv -f src/redis-server ${BIN_DIR}/redis-server
+mv -f src/redis-sentinel ${BIN_DIR}/redis-sentinel
+
+cd ${ROOT_DIR}
+./bin/redis-server conf/redis-master.conf &
+./bin/redis-server conf/redis-slave-1.conf &
+./bin/redis-server conf/redis-slave-2.conf &
+./bin/redis-sentinel conf/sentinel-1.conf &
+./bin/redis-sentinel conf/sentinel-2.conf &
+./bin/redis-sentinel conf/sentinel-3.conf &
